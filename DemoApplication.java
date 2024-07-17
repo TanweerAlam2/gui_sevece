ee305ccb9297083d89fd78c6f23a23630935349d
TanweerAlam2 <157495238+TanweerAlam2@users.noreply.github.com>
2024-07-17 12:49:58 -0700

    Add files via upload

---
+++ DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		//System.out.println("The times of india");
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
